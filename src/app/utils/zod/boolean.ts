9fb0fb44cb224ddb809b83abb76e82fd7ddf6809
mostafizurrahaman <mostafizurrahaman0401@gmail.com>
2026-09-21 08:28:56 +0600

    feat: completed successfully

--- src/app/utils/zod/boolean.ts
+++ src/app/utils/zod/boolean.ts
@@ -1,16 +1,28 @@
 import z from "zod";
 
 export const requiredStrBoolean = (fieldName: string) =>
-   z.union([
+  z.union(
+    [
       z.boolean({
-         error: `${fieldName} is should be boolean.`,
+        error: `${fieldName} is should be boolean.`,
       }),
       z
-         .string({
-            error: `${fieldName} should be true/false.`,
-         })
-         .refine((value) => value === "true" || value === "false", {
-            error: `${fieldName} should be true/false.`,
-         })
-         .transform((value) => value === "true"),
-   ]);
+        .string({
+          error: `${fieldName} should be true/false.`,
+        })
+        .refine((value) => value === "true" || value === "false", {
+          error: `${fieldName} should be true/false.`,
+        })
+        .transform((value, ctx) => {
+          if (value === "true") return true;
+          if (value === "false") return false;
+          ctx.addIssue({
+            code: "custom",
+            message: `${fieldName} should be true/false.`,
+          });
+        }),
+    ],
+    {
+      error: `${fieldName} should be true/false.`,
+    },
+  );
